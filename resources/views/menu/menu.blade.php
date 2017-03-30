38782c74b25399ba0c6261b4b6932d5c1b040590
QUANGNND <nnduyquang@gmail.com>
2017-03-30 22:33:19 +0700

    First

---
+++ resources/views/menu/menu.blade.php
@@ -0,0 +1,44 @@
+<div class="menubar" id="menubar">
+    <div class="menu-header">
+        <div class="container">
+            {{ Html::image('images/header/header.png','') }}
+        </div>
+        <div class="menu-info">
+            <div class="nav on-desktop visible-md-block visible-lg-block">
+                <div id="nav">
+                    <div class="container">
+                        <div class="brand">
+                            <a href="#">
+                                {{ Html::image('images/logo/smartlinks-logo.png','',array('class'=>'logo')) }}
+                            </a>
+                        </div>
+                        <div class="menu-right">
+                            <ul>
+                                <li><a href="#">Trang Chủ</a></li>
+                                <li><a href="#">Giới Thiệu</a></li>
+                                <li><a href="#">Liên Hệ</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div id="gift">
+                    <div class="container">
+                        <div class="col-md-4">
+                            <div class="one-item border1">
+                                <div class="icon-item bg-item1"></div>
+                                <div class="content-item">
+                                    <header>
+                                        <h5 class="entry-title">Google Partner</h5>
+                                    </header>
+                                    <div class="description-item">
+                                        Smartlinks là đối tác trực tiếp từ Google Partner
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
